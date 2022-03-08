0c92e60bd429ac25bed808f48cd922632fc4fd6d
Deniz <kartal.deniiz@yahoo.com>
2022-03-08 18:35:47 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] noten = { 1, 2, 3, 4, 5, 6 };
+            float anzahlschueler = 20;
+            int Notensumme = 0;
+
+            for (int i = 0; i < 6;  ++i);
+
+            {
+                
+                Notensumme = noten = *(noten );
+            }
+            float NotenDurch = 0f;
+            NotenDurch = Notensumme / anzahlschueler;
+            Console.WriteLine("Der Noten durchschnitt liegt bei:" + NotenDurch);
+        }
+    }
+}
